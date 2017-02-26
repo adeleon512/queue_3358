9ab84775e8bd47cadea07c9a91acea13db7336c2
Adam <acd21tx@gmail.com>
2017-02-26 15:16:43 -0600

    initial commit for queue_3358

---
+++ cmake-build-debug/CMakeFiles/queue_3358.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/queue_3358.dir/main.cpp.obj"
+  "queue_3358.pdb"
+  "queue_3358.exe"
+  "queue_3358.exe.manifest"
+  "libqueue_3358.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/queue_3358.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
